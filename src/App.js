e1fb88dc110d4875e0863162014e4f4cdc20a136
Axel Setyanto <laxels@gmail.com>
2018-06-01 18:40:37 -0500

    Content WIP

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import smoothScroll from 'smoothscroll';
 import profile from './profile.jpg';
 import './App.css';
 
+const Wrap = ({children}) => children;
+
 class App extends PureComponent {
   constructor(props) {
     super(props);
@@ -54,15 +56,44 @@ class App extends PureComponent {
 
   page = (page) => {
     const {activePage} = this.state;
+    let content;
+    switch (page) {
+      case 'Projects':
+        content = (<Wrap>
+          <h2>Gaudy Stuff</h2>
+          <h2>Websites</h2>
+          <h2>Plugins</h2>
+        </Wrap>);
+        break;
+      case 'About':
+        content = (<Wrap>
+          <h2>Social Media</h2>
+        </Wrap>);
+        break;
+      case 'Contact':
+        content = (<Wrap>
+          <h2>Hit me up!</h2>
+          <p>
+            <a href="mailto:laxels@gmail.com">laxels@gmail.com</a>
+            <br/>
+            Preferred method of contact
+          </p>
+          <p>
+            <a href="tel:+17132932448">(713)-293-2448</a>
+            <br/>
+            May not be consistently available
+          </p>
+        </Wrap>);
+        break;
+      default:
+    }
     return (
       <div
         key={page}
-        className={`page ${activePage === page ? 'active' : ''}`}
+        className={`page ${page}-page ${activePage === page ? 'active' : ''}`}
         style={this.transitionDelay('page', page)}
       >
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mollis dui nec sagittis imperdiet. Quisque pulvinar tempus arcu vitae elementum. Sed pulvinar vestibulum mauris, vestibulum molestie urna vehicula sit amet. Donec in laoreet neque. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec tincidunt tortor in lacus laoreet bibendum id id magna. Nam at urna hendrerit, condimentum odio ultrices, auctor risus. Curabitur interdum gravida purus, et ornare ex dignissim a. Nulla facilisi. Integer suscipit nunc et diam faucibus, ut auctor sapien auctor. Cras rutrum malesuada nulla eu ultrices. Donec eget mi lacinia, congue nunc ac, sodales quam. Ut a mauris quis nunc feugiat scelerisque. Nulla facilisi.</p>
-        <p>Pellentesque vel ante scelerisque, blandit leo non, faucibus erat. Nullam commodo tortor euismod erat laoreet tincidunt. Aliquam vitae enim a diam vulputate pharetra rhoncus eget purus. Donec sit amet commodo eros. Donec id ante a libero porta rutrum et ac ex. Sed sed posuere dui. Sed molestie tristique ullamcorper. Maecenas malesuada diam eros, a facilisis quam venenatis sed.</p>
-        <p>Curabitur in lobortis urna. Curabitur at sodales diam. Nulla rhoncus porttitor semper. Nulla facilisi. Nulla facilisi. Duis et magna sed sem blandit auctor. Proin feugiat lacus commodo nibh ultricies lobortis. Nulla consequat eu augue a fermentum. Phasellus fermentum arcu ligula, id interdum dui gravida id. Donec semper tortor sed eros venenatis, id ultricies quam scelerisque. Vivamus sed metus ut quam mollis condimentum id id neque. Nulla sodales, lacus sit amet pulvinar pharetra, ex diam tempor lectus, quis venenatis arcu velit in metus. Proin metus velit, ultricies a erat hendrerit, bibendum maximus lectus. Cras vulputate tellus at nibh lobortis, sit amet mattis libero varius. Suspendisse potenti. Suspendisse tincidunt sollicitudin erat quis iaculis.</p>
+        {content}
       </div>
     );
   }
