df30e5774edde63cc102cc20f24b766df7e0adb8
Axel Setyanto <laxels@gmail.com>
2018-06-21 10:38:29 -0700

    Tweak resume

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -79,7 +79,7 @@ class AboutPage extends PureComponent {
         <h2>Skills</h2>
         <p>Front-end web frameworks: React, Angular, Backbone</p>
         <p>Back-end web frameworks: Node.js (Express), Django</p>
-        <p>Databases: PostgreSQL, MySQL, MongoDB</p>
+        <p>Databases: PostgreSQL with PostGIS, MongoDB, MySQL</p>
         <p>Services: AWS (S3, EC2, CloudFront), Heroku, Stripe, PayPal, Facebook, SendGrid, Twilio, GitHub</p>
         <p>Servers: Apache, Nginx</p>
         <p>Languages: JavaScript, CoffeeScript, Python (familiar with Java, Haskell, PHP, C++, C, Lisp)</p>
