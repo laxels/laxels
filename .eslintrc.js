2ae25e9573a6958e6a63ab0ca60eea2420387267
axel <laxels@gmail.com>
2020-07-19 23:35:34 -0700

    standardize with laxels/cra-template

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,14 @@
 module.exports = {
-  "env": {
-    "browser": true,
-  },
-  "extends": "airbnb",
-  "parser": "babel-eslint",
-  "rules": {
-    "jsx-a11y/anchor-is-valid": 0,
-    "jsx-a11y/click-events-have-key-events": 0,
-    "jsx-a11y/no-static-element-interactions": 0,
-    "max-len": 0,
-    "no-param-reassign": 0,
-    "no-plusplus": 0,
-    "no-underscore-dangle": 0,
-    "no-use-before-define": [2, { variables: false }],
-    "object-curly-newline": 0,
-    "one-var": 0,
-    "one-var-declaration-per-line": [2, "initializations"],
-    "prefer-destructuring": [2, {
-      array: false,
-    }],
-    "react/jsx-filename-extension": 0,
-    "react/jsx-fragments": 0,
-    "react/jsx-one-expression-per-line": 0,
-    "react/jsx-props-no-spreading": 0,
-    "react/no-did-update-set-state": 0,
-    "react/no-unescaped-entities": 0,
-    "react/prop-types": 0,
-    "react/sort-comp": 0,
-  },
-  "overrides": [
-    {
-      "files": ["*.test.js"],
-      "rules": {
-        "no-undef": 0,
-      },
-    },
+  extends: [
+    'react-app',
+    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
+  rules: {
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/explicit-function-return-type': ['error'],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    'prettier/prettier': 'off',
+  },
 };
